417d2f4415fd931a9a6eda7af1add99c9fc86a58
Glitch (giphy-project-) <none>
2021-04-23 00:43:53 +0000

    📉🌈 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -3,18 +3,17 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
-let API = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+let API =
+  "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
 
-$(".search-button").click(function(){
-      fetch(API)
+$(".search-button").click(function() {
+  let userSearch = $("input").val();
+  fetch(API)
+    .then(function(response) {
+      return response.json();
+    })
 
-        .then(function(response) {
-        	return response.json();
-        })
-
-        .then(function(data) {
-        	$(".introText").append(`<img src="data.images.url">`);
-        });
+    .then(function(gifAPI) {
+      $(".introText").append(`<img src="${gifAPI.data[0].images.original.url}">`);
+    });
 });
-
-
